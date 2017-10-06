334d3cc348193f5d6bc3dbd790e11a367757e096
LuisBits <soyluisfrias@gmail.com>
2017-10-05 23:04:13 -0500

    Unfinished scripts

---
+++ guess_numbers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+import random
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    print('==== GUESS NUMBERS ====')
+    
+    main()
---
+++ palindrome.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+palindrome_checker():
+    # to lowercase
+    word = word.lower()
+
+
+if __name__ == '__main__':
+    print('=== PALINDROME CHECKER ===')
+    word = str(raw_input('Enter your word: \n'))
+    palindrome_checker(word)
