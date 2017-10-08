c0e57e37daae60698f3126de1d949504aaf1da24
luisbits <soyluisfrias@gmail.com>
2017-10-08 14:30:29 -0500

    random number script

--- guess_numbers.py
+++ guess_numbers.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
-
+"""
+1.- Ask for the number limit
+2.- Let the user find de number
+"""
 
 import random
 
 
-
 def main():
-    pass
+    number_found = False
+    random_number = random.randint(0, 20)
 
+    while not number_found:
+        number = int(raw_input("Intenta un número: \n"))
+
+        if number == random_number:
+            print("Felicidades. Encontraste el número")
+            number_found = True
+        elif number > random_number:
+            print('En número es más pequeño')
+        else:
+            print('El número es más grande')
 
 if __name__ == '__main__':
     print('==== GUESS NUMBERS ====')
-    
     main()
