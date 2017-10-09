9d2f190767f29f1be0c61f57531c190769fd6d10
luisbits <soyluisfrias@gmail.com>
2017-10-09 16:37:44 -0500

    Palindrome checker v2

--- palindrome.py
+++ palindrome.py
@@ -15,11 +15,20 @@ def palindrome_checker(word):
 
     return False
 
+
+def palindrome_light(word):
+    reversed_word = word[::-1]
+
+    if reversed_word == word:
+        return True
+    return False
+
+
 if __name__ == '__main__':
     print('=== ¿Es palíndromo? ===')
     word = str(raw_input('Escribe una palabra: \n'))
-   
-    result = palindrome_checker(word)
+
+    result = palindrome_light(word)
 
     if result is True:
         print('{} es un palíndromo'.format(word))
